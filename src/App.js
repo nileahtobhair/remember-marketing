461420a0def00bd3eecd317abca8c37bc234746d
nileahtobhair <nileahtobhair@gmail.com>
2024-04-20 22:55:42 +0200

    Add marketing main section & basic input form

--- src/App.js
+++ src/App.js
@@ -1,18 +1,20 @@
 import React from "react";
 
+import Header from "./components/Header";
+import Interest from "./components/InterestForm";
+
 import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <h1>Remember Marketing</h1>
-      </header>
-      <section className="main--container">
+      <Header />
+      {/* <section className="main--container">
         {
           "Collect your most important dates. Setup email reminders and never forget a birthday or an important date again."
         }
-      </section>
+      </section> */}
+      <Interest />
     </div>
   );
 }
---
+++ src/components/Header/Header.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+import "./header.css";
+
+function Header() {
+  return (
+    <header className="header--container">
+      <div className="header-logo">
+        <span> Remember</span>
+        <div> remember</div>
+      </div>
+    </header>
+  );
+}
+
+export default Header;
