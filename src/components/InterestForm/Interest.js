d4840ad4bd7a971be13d52b4b4e4060f86709039
nileahtobhair <nileahtobhair@gmail.com>
2024-04-21 17:52:04 +0200

    Further copy updates

--- src/components/InterestForm/Interest.js
+++ src/components/InterestForm/Interest.js
@@ -12,15 +12,20 @@ function Interest() {
           your most important dates
         </p>
       </div>
-      <p className="interest--subtext">
-        {
-          "Remember remember is coming soon! Submit your email below to get notified when we go live. Save and collect the dates most important to you and your loved ones. Setup date reminders so you’ll never miss a special day again."
-        }
-      </p>
+      <div className="interest--subtext">
+        <p>
+          {
+            "Save and collect the dates most important to you and your loved ones. Setup date reminders so you’ll never miss a special day again."
+          }
+          {
+            "Remember remember is coming soon! Submit your email below to get notified when we go live."
+          }
+        </p>
+      </div>
       <form name="remember-early" method="POST" data-netlify="true">
         <p className="form--input">
           <label>Your Email </label>
-          <input type="email" name="email" />
+          <input required type="email" name="email" />
         </p>
         <p>
           <button type="submit">Get early access</button>
